4493e2cb1c808b88b86fa081108890982ccb734a
Eddie Yeung <eycs0317@gmail.com>
2021-12-09 16:50:24 -0800

    useReducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,27 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import { View, Button, Text, StyleSheet} from 'react-native';
 
-const CounterScreen = () => {
-  const [counter, setCounter] = useState(0)
 
-  const handleClick = (e) => {
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return {count: state.count + action.payload}
+    case 'decrement':
+      return {count: state.count - action.payload}
 
-    console.log('click', e)
-    setCounter(counter + 1)
+    default:
+      return state;
   }
+
+
+}
+const CounterScreen = () => {
+  const [state, dispatch] = useReducer(reducer,{count: 0});
+  // const [counter, setCounter] = useState(0)
+
+  // const handleClick = (e) => {
+
+  //   console.log('click', e)
+  //   setCounter(counter + 1)
+  // }
   return (
     <View>
       <Button
         title='Increase'
-        onPress={() => {
-        handleClick()
-      }}/>
+        // onPress={() => {
+        //   setCounter(counter + 1)
+        // }}
+        onPress={() => dispatch({type: 'increment', payload: 1})}
+      />
       <Button
         title='Decrease'
-        onPress={() => {
-          setCounter(counter - 1)
-        }} />
-      <Text>Curretn count: {counter}</Text>
+        // onPress={() => {
+        //   setCounter(counter - 1)
+        // }}
+        onPress={() => dispatch({type: 'decrement', payload: 1})}
+      />
+
+      <Text>Curretn count: {state.count}</Text>
     </View>
   )
 }
--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,30 +1,48 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import ColorCounter from '../components/ColorCounter'
 
+const COLOR_INCREMENT = 10;
+
+const reducer = (state, action) => {
+  console.log('action', action)
+  switch(action.type) {
+    case 'red':
+      return state.red + action.amount > 255 || state.red + action.amount < 0 ? state : {...state, red: state.red + action.amount}
+    case 'green':
+      return state.green + action.amount > 255 || state.green + action.amount < 0 ? state : {...state, green: state.green + action.amount}
+    case 'blue':
+      return state.blue + action.amount > 255 || state.blue + action.amount < 0 ? state : {...state, blue: state.blue + action.amount}
+    default:
+      return state;
+  }
+}
+
 const SquareScreen = () => {
-  const [ red, setRed ] = useState(0)
-  const [ green, setGreen ] = useState(0)
-  const [ blue, setBlue ] = useState(0)
-  console.log('red', red)
-  console.log('blue', blue)
+  const [ state, dispatch ] = useReducer(reducer, {red: 0, green: 0, blue: 0})
+  console.log('state---', state)
   return (
     <View>
       <ColorCounter
-        onIncrease={() => setRed(red + 1)}
-        onDecrease={() => setRed(red - 1)}
+        onIncrease={() => dispatch({type: 'red', amount: COLOR_INCREMENT})}
+        onDecrease={() => dispatch({type: 'red', amount: -1 * COLOR_INCREMENT})}
         color='Red'
         />
       <ColorCounter
-        color='Blue'
-        onIncrease={() => setBlue(blue + 1)}
-        onDecrease={() => setBlue(blue - 1)} />
-      <ColorCounter
         color='Green'
-        onIncrease={() => setGreen(green + 1)}
-        onDecrease={() => setGreen(green - 1)}
+        onIncrease={() => dispatch({type: 'green', amount: COLOR_INCREMENT})}
+        onDecrease={() => dispatch({type: 'green', amount: -1 * COLOR_INCREMENT})}
         />
-      <View style={{height: 100, width: 100, backgroundColor: `rgb(${red}, ${blue}, ${green})`}}/>
+      <ColorCounter
+        color='Blue'
+        onIncrease={() => dispatch({type: 'blue', amount: COLOR_INCREMENT})}
+        onDecrease={() => dispatch({type: 'blue', amount: -1 * COLOR_INCREMENT})}
+        />
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${state.red}, ${state.green}, ${state.blue})`}}/>
     </View>
 
   )
